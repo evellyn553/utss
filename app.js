11b56398e65007ebd0d02925b24f62a6c5ebe827
evellyn553 <helmeee05@gmail.com>
2025-05-20 00:16:25 +0700

    menambahkan main.tf

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(Server berjalan di http://localhost:${port});
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
